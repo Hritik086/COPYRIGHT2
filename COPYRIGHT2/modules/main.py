84b3eb7ff9e855cd5f0f9d44f85e1a10c34ecea0
Hritik086 <151951067+Hritik086@users.noreply.github.com>
2024-04-01 13:53:16 +0530

    Update main.py

--- COPYRIGHT2/modules/main.py
+++ COPYRIGHT2/modules/main.py
@@ -119,7 +119,7 @@ async def handle_message(client, message):
         logging.info(f"Deleting message with ID {message.id}")
         await message.delete()
       #  user_mention = from_user.mention
-        await message.reply_text(f"@{message.from_user.username} @iCreasu marega BKL!")
+        await message.reply_text(f"@{message.from_user.username} BAAP KO BHEJ TERESE GC NA URNE WALI 🤣!")
     elif any(keyword in message.caption for keyword in FORBIDDEN_KEYWORDS):
         logging.info(f"Deleting message with ID {message.id}")
         await message.delete()
